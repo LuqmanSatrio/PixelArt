4242752d4e65089660385f1c8580da8bb32a7848
Luqman Satrio Wicaksono <satrio.luqman@gmail.com>
2018-04-10 18:04:55 +0200

    solution

--- designs.js
+++ designs.js
@@ -1,10 +1,29 @@
 // Select color input
 // Select size input
-
+const c = document.getElementById('pixelCanvas');
+const color = document.getElementById('colorPicker');
+const gridHeight = document.getElementById('inputHeight');
+const gridWidth = document.getElementById('inputWidth');
+const button = document.querySelector('#button');
 // When size is submitted by the user, call makeGrid()
+
+button.addEventListener('click',function(e) {
+	e.preventDefault();
+	makeGrid();
+});
 
 function makeGrid() {
 
-// Your code goes here!
 
+	height = gridHeight.value;
+	width = gridWidth.value;
+	 for (let i = 0; i < height; i++) {
+        let row = c.insertRow(i);
+        for (let j = 0; j < width; j++) {
+            let cell = row.insertCell(j);
+          cell.addEventListener('click', function(){
+            cell.style.backgroundColor = color.value;
+        });
+        }
+    }
 }
